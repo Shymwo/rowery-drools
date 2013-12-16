00a81eefd3463af4dc3eb26de86ab3c59100d5a9
Shymwo <szymonw8@tlen.pl>
2013-12-16 14:11:03 +0100

    init

---
+++ src/main/java/com/helper/KnowledgeBaseUtil.java
@@ -0,0 +1,32 @@
+package com.helper;
+
+import java.util.List;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+
+public class KnowledgeBaseUtil {
+
+	public static KnowledgeBase readKnowledgeBase(List<String> rules) throws Exception {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        for (String s: rules)
+        	kbuilder.add(ResourceFactory.newClassPathResource(s), ResourceType.DRL);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }
+
+}
---
+++ src/main/java/com/rowery/Main.java
@@ -0,0 +1,65 @@
+package com.rowery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.KnowledgeBase;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import com.helper.KnowledgeBaseUtil;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class Main {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+        	List<String> rules = new ArrayList<String>();
+        	rules.add("Main.drl");
+            KnowledgeBase kbase = KnowledgeBaseUtil.readKnowledgeBase(rules);
+            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            ksession.insert(message);
+            ksession.fireAllRules();
+            logger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+
+}
